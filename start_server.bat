9290a34584ab553779dcecb34d1c802fba9eff09
JamesWong2600 <mtyr35787@gmail.com>
2025-03-23 19:00:04 +0800

    changed the start_server_bat

--- start_server.bat
+++ start_server.bat
@@ -1,5 +1,21 @@
 @echo off
+REM Check if venv exists
+IF NOT EXIST "venv" (
+    echo Creating virtual environment...
+    python -m venv venv
+)
+
+REM Activate virtual environment
 call venv\Scripts\activate
+
+REM Install requirements if they exist
+IF EXIST "requirements.txt" (
+    echo Installing requirements...
+    pip install -r requirements.txt
+)
+
+REM Change directory and start server
 cd panel_project
 python manage.py runserver
+
 pause
